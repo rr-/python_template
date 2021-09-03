90280a1580bdbd331f45db124fd9948a61b1e334
rr- <rr-@sakuya.pl>
2021-09-03 22:26:21 +0200

    add missing docstrings

--- template_project/__main__.py
+++ template_project/__main__.py
@@ -1,10 +1,16 @@
+"""Main executable routine."""
 import argparse
 
 
 def parse_args() -> argparse.Namespace:
+    """Parse command line arguments.
+
+    :return: parsed command line arguments
+    """
     parser = argparse.ArgumentParser()
     return parser.parse_args()
 
 
 def main() -> None:
+    """Main script routine."""
     args = parse_args()
